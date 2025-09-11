45bb4e22aea34bc98f96a617644144f7c76b664d
rkjepso <38100136+rkjepso@users.noreply.github.com>
2025-09-11 12:50:34 +0200

    ddd

--- Foreles2_3_SimpleCallback.py
+++ Foreles2_3_SimpleCallback.py
@@ -5,10 +5,13 @@ from matplotlib.widgets import Button
 fig = plt.figure(figsize=(4,4))
 axButn = plt.axes((0.3, 0.3, 0.4, 0.4))
 btn = Button(axButn, label="Random (1..1024)", color='lightgray', hovercolor='yellow')
-
+n = 0
 def on_plot(event):
+    global n
+    n = n + 1
     nrand = randint(0, 1024)
     axButn.set_title(f'Tallet Er : {nrand}')
+    fig.suptitle(f"Clicket {n} ganger")
     plt.show()
 
 btn.on_clicked(on_plot)
