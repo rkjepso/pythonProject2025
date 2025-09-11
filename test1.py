76d19d8467c298084fd7cba0f520b02de492ae3f
rkjepso <38100136+rkjepso@users.noreply.github.com>
2025-09-11 16:15:53 +0200

    ddd

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 
 from matplotlib import pyplot as plt
+from matplotlib.pyplot import colormaps
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
@@ -8,14 +9,12 @@ import numpy as np
 from matplotlib.widgets import Button
 from matplotlib.widgets import TextBox
 df = pd.read_csv('house_data.csv')
-
 # Prepare plot
 fig = plt.figure()
 ax = fig.add_axes((.1, .2, .85, .75))
 ax.set_title("Huspriser i Bergen")
 ax.set_xlabel('Size (m2)')
 ax.set_ylabel('Standard')
-
 # X = Input/Independent y = Output
 X = df[['m2', 'Standard']]
 y = df['Price']
@@ -27,32 +26,41 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 print("Standard avvik:", int(np.sqrt(mse)))
-
 dfTest = pd.DataFrame(X_test)
 dfTest["Estimated"] = [int(e) for e in y_pred]
-estM = dfTest["Estimated"] / 1000_000
 
-def plot_bubbles():
-    colors = estM
-    scatter = ax.scatter(dfTest['m2'], dfTest['Standard'],
-                         c=colors, cmap='plasma', s=estM*60)
-    xT = dfTest['m2'].tolist()
-    yT = dfTest['Standard'].tolist()
-    eT = estM.tolist()
-    for i in range(1, len(xT)):
-        ax.text(xT[i], yT[i], s=f"{eT[i]:.1f}", color='lightgrey' if eT[i] < 10 else 'black', fontsize=12, ha='center', va='center')
-    cbar = plt.colorbar(scatter, ax=ax, pad=0.05) # Add color bar
-    cbar.set_label('Beløp (millioner)')
+# def plot_bubbles(dfPlot):
+#     estM = dfPlot["Estimated"] / 1e6
+#     xT = dfPlot['m2'].tolist()
+#     yT = dfPlot['Standard'].tolist()
+#
+#     ax.scatter(dfTest['m2'], dfTest['Standard'],
+#                          c="gray", cmap='plasma', s=estM*60)
+#     eT = estM.tolist()
+#     for i in range(1, len(xT)):
+#         ax.text(xT[i], yT[i], s=f"{eT[i]:.1f}", color='lightgrey' if eT[i] < 10 else 'black', fontsize=12, ha='center', va='center')
 
-plot_bubbles()
-
-def submit(expression):
-    ax.set_title(expression)
+#plot_bubbles(dfTest)
+def plot_bubble(m2,standard, priceM):
+    cmap = colormaps['viridis']
+    colorBack = cmap(priceM/20)
+    colorPrice = 'white'
+    if priceM > 12:
+        colorPrice = 'black'
+    ax.scatter(m2, standard, color=colorBack, s=priceM*100)
+    ax.text(m2, standard, s=f"{priceM:.1f}", color=colorPrice, fontsize=12, ha='center', va='center')
     plt.draw()
 
-axbox = fig.add_axes((0.3, 0.05, 0.4, 0.075))
+def submit(exp):
+    arr = np.fromstring(exp, sep=",")
+    XS = np.array([arr])
+    priceEst = model.predict(XS) / 1000_000
+    plot_bubble(arr[0], arr[1], priceEst[0])
+
+axbox = fig.add_axes((0.4, 0.03, 0.5, 0.06))
 text_box = TextBox(axbox, "Angi m2, standard(1-4) : ", textalignment="center")
+text_box.text_disp.set_fontsize(12)
+text_box.label.set_fontsize(12)
 text_box.on_submit(submit)
 text_box.set_val("100,2")
-
 plt.show()
