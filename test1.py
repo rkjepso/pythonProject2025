3ab780987cd93aa804429d84ca0f99f59493a72b
rkjepso <38100136+rkjepso@users.noreply.github.com>
2025-09-11 16:25:48 +0200

    ddd

--- test1.py
+++ test1.py
@@ -6,19 +6,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import pandas as pd
 import numpy as np
-from matplotlib.widgets import Button
 from matplotlib.widgets import TextBox
+
 df = pd.read_csv('house_data.csv')
 # Prepare plot
 fig = plt.figure()
 ax = fig.add_axes((.1, .2, .85, .75))
-ax.set_title("Huspriser i Bergen")
-ax.set_xlabel('Size (m2)')
-ax.set_ylabel('Standard')
+ax.set_title("Leiligheter i Bergen")
+ax.set_xlabel('(m2)')
+ax.set_ylabel('Standard (4=top,1=dårlig)')
+
 # X = Input/Independent y = Output
 X = df[['m2', 'Standard']]
 y = df['Price']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 model = LinearRegression()
 model.fit(X_train, y_train)
 
@@ -29,18 +30,6 @@ print("Standard avvik:", int(np.sqrt(mse)))
 dfTest = pd.DataFrame(X_test)
 dfTest["Estimated"] = [int(e) for e in y_pred]
 
-# def plot_bubbles(dfPlot):
-#     estM = dfPlot["Estimated"] / 1e6
-#     xT = dfPlot['m2'].tolist()
-#     yT = dfPlot['Standard'].tolist()
-#
-#     ax.scatter(dfTest['m2'], dfTest['Standard'],
-#                          c="gray", cmap='plasma', s=estM*60)
-#     eT = estM.tolist()
-#     for i in range(1, len(xT)):
-#         ax.text(xT[i], yT[i], s=f"{eT[i]:.1f}", color='lightgrey' if eT[i] < 10 else 'black', fontsize=12, ha='center', va='center')
-
-#plot_bubbles(dfTest)
 def plot_bubble(m2,standard, priceM):
     cmap = colormaps['viridis']
     colorBack = cmap(priceM/20)
@@ -63,4 +52,11 @@ text_box.text_disp.set_fontsize(12)
 text_box.label.set_fontsize(12)
 text_box.on_submit(submit)
 text_box.set_val("100,2")
+
+estM = [e / 1e6 for e in dfTest["Estimated"]] # converter til mill
+xT = dfTest['m2'].tolist()
+yT = dfTest['Standard'].tolist()
+for i in range(1, len(xT)):
+    plot_bubble(xT[i], yT[i], estM[i])
+
 plt.show()
