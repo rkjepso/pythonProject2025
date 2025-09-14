a80ae47b1e1ad962e946c497380837a921044640
rkjepso <38100136+rkjepso@users.noreply.github.com>
2025-09-14 16:04:47 +0200

    ddd

---
+++ Foreles1_4_2DLister.py
@@ -0,0 +1,9 @@
+
+
+tab = [[1 ,2, 3],[1 ,4 ,9],[1,8,27]] # liste av lister
+
+print (tab)
+print (tab[0])  # Rad
+print([i[2] for i in tab]) # Kolonne, Dette er tungvint
+
+# I leksjon introduseres numpy, et bibliotek for arbeid med data
