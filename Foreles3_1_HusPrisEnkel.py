db03ab80fa0633586788da9896c00f0ac43c3e7a
Richard Kjepso <rkj@hvl.no>
2025-09-17 19:15:14 +0200

    huspriser advanced

--- Foreles3_1_HusPrisEnkel.py
+++ Foreles3_1_HusPrisEnkel.py
@@ -1,19 +1,19 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression
-dataLeil = [[50, 1, 2.5],   [20,1,1.0],         [70, 4, 4.0],
-            [90, 3, 6.0],            [110, 2, 12.0]]
+dataLeil = [[50, 1, 2.5],   [60, 2, 4.5], [30,1,1.5],         [70, 4, 4.0],
+            [90, 3, 6.0],   [110, 2, 12.0], [50, 4, 3.5]]
 df = pd.DataFrame(columns=["m2","Std", "Price"], data=dataLeil)
 X = df[['m2','Std']]
 y = df['Price']  # Create and train the model
 model = LinearRegression()
 model.fit(X, y)
-leiligheterEst = [[100,2],[200,4], [50,4]]
+leiligheterEst = [[100,2],[100,4], [220,1], [200,4], [50,4]]
 
 dfPred = pd.DataFrame(columns=["m2","Std"], data=leiligheterEst)
 y_pred = model.predict(dfPred)
-print("Leiligheter i Bergen sentrum")
+print("Leiligheter i Bergen sentrum Standard(4=best) ")
 for i, pris in enumerate(y_pred):
-    print(f"{leiligheterEst[i][0]} estimert pris {pris:.1f}")
+    print(f"m2 {leiligheterEst[i][0]:>3} Standard {leiligheterEst[i][1]} Estimert pris {pris:.1f}")
 
-plt.show()
+
