bfe16a88eaeed4f201060e2c5c01044249b06061
Richard Kjepso <rkj@hvl.no>
2025-09-24 20:02:32 +0200

    huspriser advanced

--- TipsNedborsTeppe.py
+++ TipsNedborsTeppe.py
@@ -2,7 +2,7 @@ import random
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+#Eksempel som viser grafisk to input (uavhengige) og en output (avhengig)
 # Create meshgrid data
 x = np.linspace(-1, 1, 15)
 y = np.linspace(-1, 1, 15)
@@ -26,9 +26,12 @@ fig = plt.figure(figsize=(10, 7))
 ax = fig.add_subplot(111, projection='3d')
 
 # Use a colormap to show the gradient
+ax.grid(True)
 surf = ax.plot_surface(X, Y, Z, cmap='viridis_r', edgecolor='none', alpha=0.5)
-
+# Plot a basic wireframe.
+ax.plot_wireframe(X, Y, Z, rstride=1, cstride=1)
 # Axis labels and title
+
 ax.set_xlabel('X - Vest/Øst')
 ax.set_ylabel('Y - Nord/Sør')
 ax.set_zlabel('Z - Nedbør')
