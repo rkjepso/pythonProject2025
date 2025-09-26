17b8067f55ddad4e33111915f20a3926d0db64f1
rkjepso <38100136+rkjepso@users.noreply.github.com>
2025-09-26 17:31:45 +0200

    ddd

--- Foreles4_3_HusprisTeppe.py
+++ Foreles4_3_HusprisTeppe.py
@@ -7,15 +7,9 @@ import pandas as pd
 import numpy as np
 
 def to_rgb(value):
-    value = max(0, min(100, value))  # Clamp to [0, 100]
-    if value < 50:   # Green to Yellow
-        r = value / 50
-        g = 1.0
-    else:  # Green to Yellow
-        r = 1.0
-        g = 1 - (value - 50) / 50
-    return r, g, 0.0
-
+    if max(0, min(100, value))  < 50:
+        return value / 50,1 , 0
+    return  1.0, 1 - (value - 50) / 50, 0
 
 df = pd.read_csv('house_data.csv')
 # Prepare plot
@@ -23,7 +17,7 @@ fig = plt.figure(figsize=(10,6))
 ax = fig.add_axes((.1, .2, .85, .75), projection="3d")
 ax.set_title("Leiligheter i Bergen (Est/Real)")
 
-
+# Split the dataset
 X = df[['m2', 'Standard']]
 y = df['Price']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
@@ -35,18 +29,20 @@ y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 print("Standard avvik:", int(np.sqrt(mse)))
 
-
-x3 = np.linspace(25,250, 10)
+x3 = np.linspace(25,250, 10).tolist()
 y3 = [1,1,2,2,3,3,4,4,1,2]
 z3 = np.zeros(10)
 
+# størrelse på bar'ene
 dx = np.ones(10) * 10
 dy = np.ones(10) * 0.3
-dz = [1,2,3,4,5,6,7,8,9,10]
+dz = [1,2,3,4,5,6,7,8,9.5,10.2]
 
-zmax = max(dz)
-colors =[to_rgb(z/zmax*100) for z in dz]
+colors =[to_rgb(z/max(dz)*100) for z in dz]
 ax.bar3d(x3, y3, z3, dx, dy, dz, color=colors)
+
+for i in range(len(x3)):
+    ax.text(x=x3[i], y= y3[i], z=dz[i], s=f'{dz[i]:.1f}', fontsize=14, color='blue')
 
 ax.set_ylim(1,4)
 ax.set_xlabel('m2')
