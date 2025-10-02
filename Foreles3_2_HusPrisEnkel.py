f9c391fd3347f2740c941362d333f56906f59240
rkjepso <38100136+rkjepso@users.noreply.github.com>
2025-10-02 15:49:12 +0200

    ddd

--- Foreles3_2_HusPrisEnkel.py
+++ Foreles3_2_HusPrisEnkel.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 solgtLeil = [
-    [80,3.4,8.2],
-    [108,3.4,9.4],
-    [77,1.5,5.3],
-    [135,2.5,9.3],
-    [45,3.1,5.2]]
+    [80,    3.4,    8.2],
+    [108,   3.4,    9.4],
+    [77,    1.5,    5.3],
+    [135,   2.5,    9.3],
+    [45,    3.1,    5.2]]
 df = pd.DataFrame(columns=["m2","Std", "Price"], data=solgtLeil)
 # df = pd.read_csv('house_data.csv') # Lese huspriser fra fil/dokument
 
@@ -19,6 +19,6 @@ dfInput = pd.DataFrame(columns=["m2","Std"], data=estLeil)
 y_pred = model.predict(dfInput) # Estimerer verdien
 print("Leiligheter i Bergen sentrum Standard(4=best) ")
 for i, pris in enumerate(y_pred):
-    print(f"m2 {estLeil[i][0]:>3} Standard {estLeil[i][1]} Estimert pris {pris:.1f}")
+    print(f"m2 {estLeil[i][0]:>3} Standard {estLeil[i][1]} Estimert pris {pris:>5.1f}")
 
 
